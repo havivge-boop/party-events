e23d46c0426dfcd146e32e654c7cc82c31985e0d
havivge-boop <haviv.ge@gmail.com>
2026-07-20 22:46:07 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Heebo } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const heebo = Heebo({
@@ -17,7 +18,10 @@ export default function RootLayout({ children }) {
       lang="he"
       dir="rtl"
       className={`${heebo.variable} h-full antialiased`}    >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
